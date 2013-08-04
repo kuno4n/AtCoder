553253af5744b2d390cfb42aa01d3581e5e8649f
kuno4n <kuno4n@gmail.com>
2013-08-04 17:39:35 +0900

    20130804

--- src/tenka1-2013-qualA-D.cpp
+++ src/tenka1-2013-qualA-D.cpp
@@ -33,72 +33,104 @@ using namespace std;
 #define FIT(it,v) for (__typeof((v).begin()) it = (v).begin(); it != (v).end(); it++)
 #define OUT(A) cout << #A << " = "<< (A) << endl
 #define OUT2(A, B) cout << "(" << #A << ", " << #B << ") = (" << (A) << ", "<< (B) << ")" << endl
-template<class T> void chmin(T &t, T f) { if (t > f) t = f; }
-template<class T> void chmax(T &t, T f) { if (t < f) t = f; }
+template<class T> void chmin(T &t, T f) { if (t > f) t = f; } 
+template<class T> void chmax(T &t, T f) { if (t < f) t = f; } 
 
 #define present(c, e) ((c).find((e)) != (c).end())
 #define cpresent(c, e) (find(all(c), (e)) != (c).end())
 
 
-const int INF = (1<<30);
 
 
-LL str2int(string s){
-    LL res = 0;
-    REP(i, SZ(s)){
-        res *= 10;
-        res += s[i]-'0';
-    }
-    return res;
+struct Node {
+	string s;
+	LL len, repeat;
+	vector<Node *> children;
+	Node(){
+		len = 0, repeat = 0;
+	}
+};
+
+
+pair<Node *, int> build(const char *s, int p){
+	Node *n = new Node();
+	while(s[p] != ')' && s[p] != '\0'){
+		if(s[p] == '('){
+			pair<Node *, int> q = build(s, p+1);
+			Node *m = q.first;
+			p = q.second + 1;
+			m->repeat = 0;
+			while(isdigit(s[p])){
+				m->repeat *= 10;
+				m->repeat += s[p++] - '0';
+			}
+			m->len *= m->repeat;
+			if(m->repeat == 1) REP(i, SZ(m->children)) n->children.push_back(m->children[i]);
+			else n->children.push_back(m);
+		}
+		else{
+			Node *m = new Node();
+			while(s[p] != '(' && s[p] != ')' && s[p] != '\0'){
+				m->s += s[p++];
+				m->len++;
+			}
+			m->repeat = 1;
+			n->children.push_back(m);
+		}
+	}
+	REP(i, SZ(n->children)) n->len += n->children[i]->len;
+	n->repeat = 1;
+	return make_pair(n, p);
 }
 
-int b, l, n;
-string s;
-string stk[1010];
 
-void init() {
+void dump(const Node *node, int depth = 0){
+	cout << string(depth * 2, ' ');
+	if(node->children.empty()){
+		cout << "\"" << node->s << "\"" << " " << node->len << endl;
+	}else{
+		cout << "(" << node->len << ", " << node->repeat << ")" << endl;
+		REP(i, SZ(node->children)){
+			dump(node->children[i], depth + 1);
+		}
+	}
 }
 
-void input() {
-    cin >> b >> l >> n >> s;
+char get(const Node *node, LL pos){
+	if(SZ(node->children) == 0) return node->s[pos];
+	LL sum = 0;
+	REP(i, SZ(node->children)){
+		Node *n = node->children[i];
+		if(pos < sum + n->len) return get(n, (pos-sum) % (n->len/n->repeat));
+		sum += n->len;
+	}
+	return ' ';
 }
 
 void solve() {
-    int pt = 0, st = 0;
-    REP(i, 1010) stk[i] = "";
-    
-    while(pt < n){
-        if(s[pt] == '('){
-            pt++;
-            st++;
-            stk[st] = "";
-        }
-        else if(s[pt] == ')'){
-            pt++;
-            st--;
-            string pos = "";
-            while(pt < n && s[pt] >= '0' && s[pt] <= '9'){
-                pos += s[pt++];
-            }
-            int itr = str2int(pos);
-            REP(_, itr) stk[st] += stk[st+1];
-        }
-        else{
-            stk[st] += s[pt];
-            pt++;
-        }
-    }
-    
-    int m = SZ(stk[0]);
-    REP(i, l){
-        cout << stk[0][(m+b+i)%m];
-    }
-    cout << endl;
-    
+	
+	LL b, l, n;
+	string s;
+	cin >> b >> l >> n >> s ;
+	
+	Node *root = build(s.c_str(), 0).first;
+//	dump(root);
+	LL allLen = root->len;
+	
+	string res = "";
+	REP(i, l) res += get(root, (b+i+allLen) % allLen);
+	cout << res << endl;
+	
+
 }
 
 int main() {
-    init();
-    input();
     solve();
+	return 0;
 }
+
+
+
+
+
+

06361587a2de35a1e8540f939247b2a087cea091
kuno.shinya <kuno4n@gmail.com>
2019-02-12 11:41:33 +0900

    20190212

---
+++ src/ABC-117-D.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <numeric>
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cctype>
+#include <cmath>
+#include <cassert>
+#include <cstdarg>
+#include <sys/time.h>
+#include <fstream>
+//#include "cout.h"
+
+using namespace std;
+
+#define SZ(x) ((int)x.size())
+#define MSET(x,a) memset(x, a, (int)sizeof(x))
+#define PB push_back
+#define VI vector < int >
+#define PII pair < int, int >
+#define LL long long
+#define FOR(i,a,b) for (int i = (a); i < (b); i++)
+#define REP(i,n) FOR(i,0,n)
+#define ALL(v) (v).begin(), (v).end()
+#define FIT(it,v) for (__typeof((v).begin()) it = (v).begin(); it != (v).end(); it++)
+#define OUT(A) cout << #A << " = "<< (A) << endl
+#define OUT2(A, B) cout << "(" << #A << ", " << #B << ") = (" << (A) << ", "<< (B) << ")" << endl
+template<class T> void chmin(T &t, T f) { if (t > f) t = f; }
+template<class T> void chmax(T &t, T f) { if (t < f) t = f; }
+
+#define present(c, e) ((c).find((e)) != (c).end())
+#define cpresent(c, e) (find(ALL(c), (e)) != (c).end())
+
+LL n, k;
+LL a[100010];
+
+void init() {
+
+}
+
+void input() {
+  cin >> n >> k;
+  REP(i, n) cin >> a[i];
+}
+
+void solve() {
+  LL max = 0;
+  for(int i = 50; i >= 0; i--) {
+    LL bit = 1LL << i;
+    int cnt = 0;
+    REP(j, n) if(a[j] & bit) cnt++;
+    if (cnt <= n/2 && max+bit <= k) max += bit;
+  }
+  LL res = 0;
+  REP(i, n) res += max^a[i];
+  cout << res << endl;
+}
+
+int main() {
+	init();
+	input();
+	solve();
+	return 0;
+}
